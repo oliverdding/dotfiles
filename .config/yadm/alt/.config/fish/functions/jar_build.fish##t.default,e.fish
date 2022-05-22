fccfb6cd0b72ed081f6756604ec0fcfc02bf2d96
oliverdding <oliverdding@outlook.com>
2022-05-22 15:07:04 +0800

    update

--- .config/yadm/alt/.config/fish/functions/jar_build.fish##t.default,e.fish
+++ .config/yadm/alt/.config/fish/functions/jar_build.fish##t.default,e.fish
@@ -3,6 +3,7 @@ function jar_build --description 'assembly jar file of QAPM/emr'
         echo "🤔 cannot find environment variable QAPM_HOME"
         return 1
     end
+    {% if yadm.os == "Darwin" %}
     fish -c "
     cd $QAPM_HOME/emr/spark/scala/QAPMSparkProject
     sbt assembly &>/dev/null
@@ -24,4 +25,15 @@ function jar_build --description 'assembly jar file of QAPM/emr'
                 -message "bujar failed with error code $statue"
         end
     end
+    {% else %}
+    fish -c "
+    cd $QAPM_HOME/emr/spark/scala/QAPMSparkProject
+    sbt assembly
+    "
+    if test $status = 0
+        echo "🥳 bujar finished with assembly file at $QAPM_HOME/emr/spark/scala/QAPMSparkProject/target/scala-2.11/QAPMSparkProject-assembly-6.1.jar"
+    else
+        echo "☹️ bujar failed with error code $statue"
+    end
+    {% endif %}
 end
--- .config/yadm/alt/.config/fish/functions/jar_upload.fish##o.Darwin,e.fish
+++ .config/yadm/alt/.config/fish/functions/jar_upload.fish##o.Darwin,e.fish
@@ -4,23 +4,24 @@ function jar_upload -a cluster -d "upload jar file of QAPM/emr to HDFS"
         return 1
     end
 
-    if not contains $cluster "tencent_public" "cloud_public" "qq" "gray" "test"
+    if not contains $cluster tencent_public cloud_public qq gray test
         echo "Unknown target cluster '$cluster', only support 'tencent', 'public', 'qq', 'gray' and 'test' now."
         return 2
     end
     set -f JAR_PATH "/bin/tdem/$cluster/spark/QAPMSparkProject-assembly-6.1.jar"
 
+    {% if yadm.os == "Darwin" %}
     upload2hdfs $QAPM_HOME/emr/spark/scala/QAPMSparkProject/target/scala-2.11/QAPMSparkProject-assembly-6.1.jar $JAR_PATH &
     function upjar_ended --on-process-exit $last_pid
         if test $argv[3] = 0
-            terminal-notifier -title 'upjar' \
+            terminal-notifier -title upjar \
                 -subtitle '🥳' \
                 -group qapm \
                 -ignoreDnD \
                 -sender $__CFBundleIdentifier \
                 -message "upjar finished with jar file to cluster $cluster"
         else
-            terminal-notifier -title 'upjar' \
+            terminal-notifier -title upjar \
                 -subtitle '☹️' \
                 -group qapm \
                 -ignoreDnD \
@@ -28,4 +29,12 @@ function jar_upload -a cluster -d "upload jar file of QAPM/emr to HDFS"
                 -message "upjar failed with error code $statue"
         end
     end
+    {% else %}
+    upload2hdfs $QAPM_HOME/emr/spark/scala/QAPMSparkProject/target/scala-2.11/QAPMSparkProject-assembly-6.1.jar $JAR_PATH
+    if test $status = 0
+        echo "🥳 upjar finished with jar file to cluster $cluster"
+    else
+        echo "☹️ upjar failed with error code $statue"
+    end
+    {% endif %}
 end
