b95ff4baee90929f4b7476e47a36cba37d8b248b
oliverdding <oliverdding@outlook.com>
2022-05-22 11:26:34 +0800

    update

--- .config/yadm/alt/.config/fish/functions/jar_upload.fish##default,e.fish
+++ .config/yadm/alt/.config/fish/functions/jar_upload.fish##default,e.fish
@@ -10,36 +10,7 @@ function jar_upload -a cluster -d "upload jar file of QAPM/emr to HDFS"
     end
     set -f JAR_PATH "/bin/tdem/$cluster/spark/QAPMSparkProject-assembly-6.1.jar"
 
-    function upload_file -a src_path,dst_path
-        set -l CLIENT_POD (kp -n default get pods | rg 'kdl-client' | head -1 | awk '{print $1}')
-        if not set -q CLIENT_POD
-            echo "Cannot get client pod, is it running?"
-            return 3
-        end
-        if test ! -e $src_path
-            echo "Cannot find source file, does it exist?"
-            return 4
-        end
-
-        if test INTRANET_PROXY_JUMP_SERVERS = ''
-            kubectl -n default cp $src_path $CLIENT_POD:"/tmp/upload_by_$HOST"
-            kubectl -n default exec $CLIENT_POD -- bash -c "hdfs --config /etc/hadoop-custom-conf/ dfs -mkdir -p $(dirname $dst_path)"
-            kubectl -n default exec $CLIENT_POD -- bash -c "hdfs --config /etc/hadoop-custom-conf/ dfs -rm -f $dst_path"
-            kubectl -n default exec $CLIENT_POD -- bash -c "hdfs --config /etc/hadoop-custom-conf/ dfs -put /tmp/upload_by_$HOST $dst_path"
-        else
-            scp $src_path $INTRANET_PROXY_JUMP_SERVERS:"/tmp/upload_by_$HOST"
-            ssh $INTRANET_PROXY_JUMP_SERVERS "fish -c \"
-            kubectl -n default cp "/tmp/upload_by_$HOST" $CLIENT_POD:"/tmp/upload_by_$HOST"
-            kubectl -n default exec $CLIENT_POD -- bash -c \"hdfs --config /etc/hadoop-custom-conf/ dfs -mkdir -p $(dirname $dst_path)\"
-            kubectl -n default exec $CLIENT_POD -- bash -c \"hdfs --config /etc/hadoop-custom-conf/ dfs -rm -f $dst_path\"
-            kubectl -n default exec $CLIENT_POD -- bash -c \"hdfs --config /etc/hadoop-custom-conf/ dfs -put /tmp/upload_by_$HOST $dst_path\"
-            \"
-            "
-        end
-        functions -e upload_file
-    end
-
-    upload_file $QAPM_HOME/emr/spark/scala/QAPMSparkProject/target/scala-2.11/QAPMSparkProject-assembly-6.1.jar $JAR_PATH
+    upload2hdfs $QAPM_HOME/emr/spark/scala/QAPMSparkProject/target/scala-2.11/QAPMSparkProject-assembly-6.1.jar $JAR_PATH
     if test $status = 0
         echo "🥳 upjar finished with jar file to cluster $cluster"
     else
---
+++ .config/yadm/alt/.config/fish/functions/upload2hdfs.fish##t.default,e.fish
@@ -0,0 +1,27 @@
+function upload2hdfs -a src_path,dst_path -d "upload file to HDFS with proxy jump"
+    set -l CLIENT_POD (kp -n default get pods | rg 'kdl-client' | head -1 | awk '{print $1}')
+    if not set -q CLIENT_POD
+        echo "Cannot get client pod, is it running?"
+        return 1
+    end
+    if test ! -e $src_path
+        echo "Cannot find source file, does it exist?"
+        return 2
+    end
+
+    if test INTRANET_PROXY_JUMP_SERVERS = ''
+        kubectl -n default cp $src_path $CLIENT_POD:"/tmp/upload_by_$HOST"
+        kubectl -n default exec $CLIENT_POD -- bash -c "hdfs --config /etc/hadoop-custom-conf/ dfs -mkdir -p $(dirname $dst_path)"
+        kubectl -n default exec $CLIENT_POD -- bash -c "hdfs --config /etc/hadoop-custom-conf/ dfs -rm -f $dst_path"
+        kubectl -n default exec $CLIENT_POD -- bash -c "hdfs --config /etc/hadoop-custom-conf/ dfs -put /tmp/upload_by_$HOST $dst_path"
+    else
+        scp $src_path $INTRANET_PROXY_JUMP_SERVERS:"/tmp/upload_by_$HOST"
+        ssh $INTRANET_PROXY_JUMP_SERVERS "fish -c \"
+        kubectl -n default cp "/tmp/upload_by_$HOST" $CLIENT_POD:"/tmp/upload_by_$HOST"
+        kubectl -n default exec $CLIENT_POD -- bash -c \"hdfs --config /etc/hadoop-custom-conf/ dfs -mkdir -p $(dirname $dst_path)\"
+        kubectl -n default exec $CLIENT_POD -- bash -c \"hdfs --config /etc/hadoop-custom-conf/ dfs -rm -f $dst_path\"
+        kubectl -n default exec $CLIENT_POD -- bash -c \"hdfs --config /etc/hadoop-custom-conf/ dfs -put /tmp/upload_by_$HOST $dst_path\"
+        \"
+        "
+    end
+end
