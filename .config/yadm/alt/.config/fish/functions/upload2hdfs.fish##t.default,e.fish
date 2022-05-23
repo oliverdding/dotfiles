8320b5990d15b759faeacdff80f726bfec1b9977
oliverdding <oliverdding@outlook.com>
2022-05-23 11:10:11 +0800

    update

--- .config/yadm/alt/.config/fish/functions/upload2hdfs.fish##t.default,e.fish
+++ .config/yadm/alt/.config/fish/functions/upload2hdfs.fish##t.default,e.fish
@@ -1,5 +1,5 @@
 function upload2hdfs -a src_path dst_path -d "upload file to HDFS with proxy jump"
-    set -f CLIENT_POD (ssh $INTRANET_PROXY_JUMP_SERVERS "exec kubectl -n default get pods" | rg 'kdl-client' | head -1 | awk '{print $1}')
+    set -f CLIENT_POD (ssh devcloud "exec kubectl -n default get pods" | rg 'kdl-client' | head -1 | awk '{print $1}')
     if not set -q CLIENT_POD
         echo "Cannot get client pod, is it running?"
         return 1
@@ -12,7 +12,7 @@ function upload2hdfs -a src_path dst_path -d "upload file to HDFS with proxy jum
 {% if yadm.hostname == "devcloud" %}
     cp $src_path "/tmp/file_to_hdfs_by_$(uname -n)"
 {% else %}
-    scp $src_path $INTRANET_PROXY_JUMP_SERVERS:"/tmp/upload_by_$(uname -n)"
+    scp $src_path devcloud:"/tmp/upload_by_$(uname -n)"
 {% endif %}
     execute_in_devcloud bash -c "'
     kubectl -n default cp \"/tmp/file_to_hdfs_by_$HOST\" $CLIENT_POD:\"/tmp/file_to_hdfs_by_$(uname -n)\"
