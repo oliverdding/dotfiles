ae1ca627de1fb2539ae5be9d0fb3808acc0e73ea
oliver ding <oliverdding@outlook.com>
2022-05-24 19:36:41 +0800

    add GPG_AGENT_INFO

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,5 @@
 set -q SSH_AGENT_PID && unset SSH_AGENT_PID
 set -gx SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
 set -gx GPG_TTY (tty)
+set -gx GPG_AGENT_INFO "$(gpgconf --list-dirs agent-socket):0:1"
 gpg-connect-agent updatestartuptty /bye >/dev/null
