7f39381e4715d36cf32532692ea33085bfc18a9a
oliver ding <oliverdding@outlook.com>
2022-05-24 09:09:35 +0800

    enable gpg ssh support

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,2 +1,4 @@
+test -q SSH_AGENT_PID && unset SSH_AGENT_PID
+set -gx SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
 set -gx GPG_TTYP (tty)
 gpg-connect-agent updatestartuptty /bye >/dev/null
