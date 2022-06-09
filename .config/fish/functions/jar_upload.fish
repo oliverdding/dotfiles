3c3adcc20078a2004b1cf19b1954935058094e1d
oliver ding <oliverdding@outlook.com>
2022-06-10 00:27:55 +0800

    update

--- .config/fish/functions/jar_upload.fish
+++ .config/fish/functions/jar_upload.fish
@@ -12,34 +12,10 @@ function jar_upload -a cluster -d "upload jar file of QAPM/emr to HDFS"
     if ! confirmation_make "Are your sure to upload assemblied jar to HDFS on cluster $cluster?"
         return 3
     end
-
-{% if yadm.os == "Darwin" %}
-    fish -c "
-    upload2hdfs $QAPM_HOME/emr/spark/scala/QAPMSparkProject/target/scala-2.11/QAPMSparkProject-assembly-6.1.jar $JAR_PATH
-    "  &>/dev/null &
-    function upjar_ended --on-process-exit $last_pid
-        if test $argv[3] = 0
-            terminal-notifier -title "upload jar" \
-                -subtitle '🥳' \
-                -group qapm \
-                -ignoreDnD \
-                -sender $__CFBundleIdentifier \
-                -message "jar_upload finished with jar file to cluster $cluster"
-        else
-            terminal-notifier -title "upload jar" \
-                -subtitle '☹️' \
-                -group qapm \
-                -ignoreDnD \
-                -sender $__CFBundleIdentifier \
-                -message "jar_upload failed with error code $statue"
-        end
-    end
-{% else %}
     upload2hdfs $QAPM_HOME/emr/spark/scala/QAPMSparkProject/target/scala-2.11/QAPMSparkProject-assembly-6.1.jar $JAR_PATH
     if test $status = 0
         echo "🥳 jar_upload finished with jar file to cluster $cluster"
     else
         echo "☹️ jar_upload failed with error code $statue"
     end
-{% endif %}
 end
