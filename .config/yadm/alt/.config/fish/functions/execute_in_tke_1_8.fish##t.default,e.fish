42f1ce3f90f040c8070084b60195eb02d8bed3c5
oliver ding <oliverdding@outlook.com>
2022-06-28 11:23:39 +0800

    update

--- .config/yadm/alt/.config/fish/functions/execute_in_tke_1_8.fish##t.default,e.fish
+++ .config/yadm/alt/.config/fish/functions/execute_in_tke_1_8.fish##t.default,e.fish
@@ -1,4 +1,4 @@
-function execute_in_intranet -a command -d "execute command in intranet"
+function execute_in_tke_1_8 -a command -d "execute command in tke1.8"
     {% if yadm.hostname == "devcloud" %}
     ssh -J $INTRANET_PROXY_JUMP_SERVER -t $TKE_1_8_PROXY_JUMP_SERVER "exec $argv"
     {% else %}
