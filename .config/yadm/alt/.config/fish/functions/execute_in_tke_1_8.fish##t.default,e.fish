df926a1ee60a27b551397a17ab78cd94258c9462
oliver ding <oliverdding@outlook.com>
2022-06-28 11:25:26 +0800

    fix bugs in execute_in_tke_1_8

--- .config/yadm/alt/.config/fish/functions/execute_in_tke_1_8.fish##t.default,e.fish
+++ .config/yadm/alt/.config/fish/functions/execute_in_tke_1_8.fish##t.default,e.fish
@@ -1,7 +1,7 @@
 function execute_in_tke_1_8 -a command -d "execute command in tke1.8"
-    {% if yadm.hostname == "devcloud" %}
+{% if yadm.hostname == "devcloud" %}
     ssh -J $INTRANET_PROXY_JUMP_SERVER -t $TKE_1_8_PROXY_JUMP_SERVER "exec $argv"
-    {% else %}
+{% else %}
     ssh -J devcloud,$INTRANET_PROXY_JUMP_SERVER -t $TKE_1_8_PROXY_JUMP_SERVER "exec $argv"
-    {% endif %}
+{% endif %}
 end
