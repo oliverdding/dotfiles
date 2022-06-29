8e89c1f43411dae85724ed2c45d49b264a4de086
oliver ding <oliverdding@outlook.com>
2022-06-29 16:19:36 +0800

    update img_encode: copy from

--- .config/yadm/alt/.config/fish/functions/img_encode.fish##o.Darwin,e.fish
+++ .config/yadm/alt/.config/fish/functions/img_encode.fish##o.Darwin,e.fish
@@ -1,3 +1,7 @@
 function img_encode -a source_image -d "encode image with base64 and copy into clipboard"
-    echo -e "data:image/*;base64,"(base64 -i $source_image) | pbcopy
+    if set -q source_image; and string length -q -- $source_image; and test -e $source_image
+        echo -e "data:image/*;base64,"(base64 -i $source_image) | pbcopy
+    else
+        echo -e "data:image/*;base64,"(pngpaste -b) | pbcopy
+    end
 end
