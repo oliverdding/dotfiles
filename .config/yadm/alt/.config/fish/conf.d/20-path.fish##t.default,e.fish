421d1d7b9d10cbd256b39027a228e5fa46e83bd0
oliver ding <oliverdding@outlook.com>
2022-06-30 11:31:52 +0800

    update npm

--- .config/yadm/alt/.config/fish/conf.d/20-path.fish##t.default,e.fish
+++ .config/yadm/alt/.config/fish/conf.d/20-path.fish##t.default,e.fish
@@ -7,8 +7,9 @@ fish_add_path -g $HOME/.cargo/bin
 fish_add_path -g $HOME/.local/share/go/bin
 {% if yadm.os == "Darwin" %}
 fish_add_path -g (ls -1dD $HOME/Library/Python/* 2>/dev/null | sed 's#$#/bin#')
+fish_add_path -g $HOME/.npm-global/bin
 {% else %}
 fish_add_path -g (ls -1dD $HOME/.local/lib/python*/* 2>/dev/null | sed 's#$#/bin#')
+fish_add_path -g $XDG_DATA_HOME/npm/bin
 {% endif %}
-fish_add_path -g $HOME/.npm/bin
 fish_add_path -g $HOME/.krew/bin
