2d59781e6c3fe4f8cf51ddc1f58e2dbffcb76ea0
oliver ding <oliverdding@outlook.com>
2022-07-08 16:37:05 +0800

    update

--- .config/yadm/alt/.bashrc##o.Darwin,e.sh
+++ .config/yadm/alt/.bashrc##o.Darwin,e.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-[[ $PS1 ]] && [[ -f /opt/local/etc/profile ]] && . /opt/local/etc/profile
+[[ $PS1 ]] & [[ -f /opt/local/etc/profile ]] && . /opt/local/etc/profile
 
 [[ $PS1 ]] && gpg-connect-agent updatestartuptty /bye >/dev/null
 
-[[ $PS1 ]] && source /opt/local/share/fzf/shell/key-bindings.bash
+[[ $PS1 ]] & [[ -f /opt/local/share/fzf/shell/key-bindings.bash ]] && source /opt/local/share/fzf/shell/key-bindings.bash
 
 [[ $PS1 ]] && eval "$(zoxide init bash)"
 
