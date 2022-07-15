28b2c03aaf049c4091ff91cafacee1894ac8f4e5
oliver ding <oliverdding@outlook.com>
2022-07-15 18:45:12 +0800

    Update

--- .config/yadm/alt/.bashrc##o.Linux,e.sh
+++ .config/yadm/alt/.bashrc##o.Linux,e.sh
@@ -2,7 +2,7 @@
 
 [[ $PS1 ]] && gpg-connect-agent updatestartuptty /bye >/dev/null
 
-[[ $PS1 ]] & [[ -f /usr/share/fzf/key-bindings.bash ]] && source /usr/share/fzf/key-bindings.bash
+[[ $PS1 && -f /usr/share/fzf/key-bindings.bash ]] && source /usr/share/fzf/key-bindings.bash
 
 [[ $PS1 ]] && eval "$(zoxide init bash)"
 
