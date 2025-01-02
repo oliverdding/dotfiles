46c929c8f40dee9893f87999067a810a9f4f3fed
oliverdding <oliverdding@tencent.com>
2025-01-02 16:53:09 +0800

    feat: add cargo-show-asm

--- dot_config/aconfmgr/152-install-development-rust.sh
+++ dot_config/aconfmgr/152-install-development-rust.sh
@@ -2,6 +2,7 @@ AddPackage cargo-audit # Audit Cargo.lock for crates with security vulnerabiliti
 AddPackage cargo-binstall # Binary installation for Rust projects
 AddPackage cargo-generate # Use pre-existing git repositories as templates
 AddPackage cargo-outdated # A cargo subcommand for displaying when Rust dependencies are out of date
+AddPackage cargo-show-asm # Cargo subcommand to display ASM, LLVM-IR and MIR for Rust source code
 AddPackage cargo-update # A cargo subcommand for checking and applying updates to installed executables
 AddPackage cargo-wizard # Cargo subcommand for configuring Cargo profile
 AddPackage rustup # The Rust toolchain installer
