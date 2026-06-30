dbfee4b0e729c7f9045e0db2be93406084e6b4fc
oliver ding <oliverdding@outlook.com>
2026-06-30 20:37:13 +0800

    feat(linux): add systemd-resolved for dns caching and DoT

--- dot_config/aconfmgr/300-configure-network.sh
+++ dot_config/aconfmgr/300-configure-network.sh
@@ -7,3 +7,7 @@ CopyFile /etc/NetworkManager/conf.d/90-dns-servers.conf # https://wiki.archlinux
 CreateLink /etc/systemd/system/dbus-org.freedesktop.nm-dispatcher.service /usr/lib/systemd/system/NetworkManager-dispatcher.service
 CreateLink /etc/systemd/system/multi-user.target.wants/NetworkManager.service /usr/lib/systemd/system/NetworkManager.service
 CreateLink /etc/systemd/system/network-online.target.wants/NetworkManager-wait-online.service /usr/lib/systemd/system/NetworkManager-wait-online.service
+CreateLink /etc/systemd/system/dbus-org.freedesktop.resolve1.service /usr/lib/systemd/system/systemd-resolved.service
+CreateLink /etc/systemd/system/sysinit.target.wants/systemd-resolved.service /usr/lib/systemd/system/systemd-resolved.service
+CreateLink /etc/systemd/system/sockets.target.wants/systemd-resolved-varlink.socket /usr/lib/systemd/system/systemd-resolved-varlink.socket
+CreateLink /etc/systemd/system/sockets.target.wants/systemd-resolved-monitor.socket /usr/lib/systemd/system/systemd-resolved-monitor.socket
